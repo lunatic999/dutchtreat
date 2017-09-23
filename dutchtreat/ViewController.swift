becaf60622011bc61ef1d86058abcf17dd522130
federiconessi <federico.nessi@funambol.com>
2017-09-23 18:26:11 +0200

    [TASK] First commit - application skeleton

---
+++ dutchtreat/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  dutchtreat
+//
+//  Created by Federico Nessi on 23/09/2017.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
